55ce6685ceb1c629c09629cb83970b8c0c4ac194
jonahshader <jonah.shader@gmail.com>
2025-03-18 20:22:02 -0400

    add tile-player collision handling

--- sim/src/core.cpp
+++ sim/src/core.cpp
@@ -14,7 +14,7 @@ constexpr Tile read_base_map(int x, int y) {
 }
 
 void update_player(Player &p, const PlayerInput &input) {
-  constexpr Fixed3 FIXED3_ZERO = Fixed3::from_raw(0);
+  constexpr Fixed3 F3_ZERO = Fixed3::from_raw(0);
 
   const int x_low = p.x.to_integer_floor();
   const int x_high = p.x.to_integer_ceil();
@@ -82,21 +82,21 @@ void update_player(Player &p, const PlayerInput &input) {
     }
   } else {
     // decelerate towards zero
-    if (p.x_vel > FIXED3_ZERO) {
+    if (p.x_vel > F3_ZERO) {
       // check if we have room to do the full speed reduction
       if (p.x_vel >= move_accel) {
         p.x_vel -= move_accel;
       } else {
         // we are too slow to do the full speed reduction
-        p.x_vel = FIXED3_ZERO;
+        p.x_vel = F3_ZERO;
       }
-    } else if (p.x_vel < FIXED3_ZERO) {
+    } else if (p.x_vel < F3_ZERO) {
       // check if we have room to do the full speed reduction
       if (p.x_vel <= -move_accel) {
         p.x_vel += move_accel;
       } else {
         // we are too slow to do the full speed reduction
-        p.x_vel = FIXED3_ZERO;
+        p.x_vel = F3_ZERO;
       }
     }
   }
@@ -105,11 +105,70 @@ void update_player(Player &p, const PlayerInput &input) {
   p.x += p.x_vel;
   p.y += p.y_vel;
 
-  // TODO: handle collisions against solid tiles
+  // handle collisions against solid tiles
+  const int xn_low = p.x.to_integer_floor();
+  const int xn_high = p.x.to_integer_ceil();
+  const int yn_low = p.y.to_integer_floor();
+  const int yn_high = p.y.to_integer_ceil();
 
+  const int xn_tile_left = xn_low / CELL_SIZE;
+  const int xn_tile_right = (xn_high + PLAYER_WIDTH - 1) / CELL_SIZE;
+  const int yn_tile_down = yn_low / CELL_SIZE;
+  const int yn_tile_up = (yn_high + PLAYER_HEIGHT - 1) / CELL_SIZE;
+
+  // handle left right collisions
+  if (p.x_vel < F3_ZERO) {
+    // going left. check left side
+    const Tile left_1 = read_base_map(xn_tile_left, y_tile_down);
+    const Tile left_2 = read_base_map(xn_tile_left, y_tile_up);
+    if (is_solid(left_1) || is_solid(left_2)) {
+      // make flush with wall
+      p.x = Fixed3(static_cast<int16_t>((xn_tile_left + 1) * CELL_SIZE));
+      // cancel velocity
+      p.x_vel = F3_ZERO;
+    }
+  } else if (p.x_vel > F3_ZERO) {
+    // going right. check right side
+    const Tile right_1 = read_base_map(xn_tile_right, y_tile_down);
+    const Tile right_2 = read_base_map(xn_tile_right, y_tile_up);
+    if (is_solid(right_1) || is_solid(right_2)) {
+      // make flush with wall
+      p.x = Fixed3(static_cast<int16_t>((xn_tile_right - 1) * CELL_SIZE));
+      // cancel velocity
+      p.x_vel = F3_ZERO;
+    }
+  }
+  if (p.y_vel < F3_ZERO) {
+    // going down. check bottom
+    const Tile down_1 = read_base_map(x_tile_left, yn_tile_down);
+    const Tile down_2 = read_base_map(x_tile_right, yn_tile_down);
+    if (is_solid(down_1) || is_solid(down_2)) {
+      // make flush with floor
+      p.y = Fixed3(static_cast<int16_t>((yn_tile_down + 1) * CELL_SIZE));
+      // cancel velocity
+      p.y_vel = F3_ZERO;
+    }
+  } else if (p.y_vel > F3_ZERO) {
+    // going up. check top
+    const Tile top_1 = read_base_map(x_tile_left, yn_tile_up);
+    const Tile top_2 = read_base_map(x_tile_right, yn_tile_up);
+    if (is_solid(top_1) || is_solid(top_2)) {
+      p.y = Fixed3(static_cast<int16_t>((yn_tile_up - 1) * CELL_SIZE));
+      // cancel velocity
+      p.y_vel = F3_ZERO;
+    }
+  }
+
+  // TODO: check coin collision.
+  // can do this in here (which would be both players in parallel in FPGA) because
+  // in the edge case where both players collide with the coin on the same frame,
+  // we'll just give both of them the point.
 }
 
 void update(GameState &state, const PlayerInput &in1, const PlayerInput &in2) {
+  // updating can happen in parallel in FPGA
+  update_player(state.p1, in1);
+  update_player(state.p2, in2);
   ++state.age;
 }
 
