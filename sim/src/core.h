ef9cfffe7b8e11383f0d24b966f7e0492a2f1aaf
jonahshader <jonah@micro-nova.com>
2025-03-24 15:34:51 -0400

    player collision working properly

--- sim/src/core.h
+++ sim/src/core.h
@@ -10,7 +10,8 @@
 
 namespace jnb {
 
-using Fixed4 = FixedPoint<int16_t, 4>;
+using F4 = FixedPoint<int16_t, 4>;
+constexpr F4 F4_ZERO = F4::from_raw(0);
 
 constexpr int CELL_SIZE = 8;
 constexpr int PLAYER_WIDTH = CELL_SIZE - 2;
@@ -23,16 +24,16 @@ constexpr int POINTS_PER_COIN = 3;
 constexpr int POINTS_PER_KILL = 1;
 constexpr int DEAD_TIMEOUT = 60; // in frames
 
-constexpr Fixed4 JUMP_VEL = Fixed4::from_raw(25);
-constexpr Fixed4 JUMP_MIDAIR_ACCEL = Fixed4::from_raw(1);
-constexpr Fixed4 SPRING_VEL = Fixed4::from_raw(38);
-constexpr Fixed4 MOVE_ACCEL = Fixed4::from_raw(3);
-constexpr Fixed4 MOVE_ACCEL_WATER = Fixed4::from_raw(2); // slower in water
-constexpr Fixed4 MOVE_ACCEL_ICE = Fixed4::from_raw(1);   // much slower on ice
-constexpr Fixed4 MOVE_MAX_VEL = Fixed4::from_raw(10);
-constexpr Fixed4 GRAVITY = Fixed4::from_raw(-2);
-constexpr Fixed4 GRAVITY_WATER = Fixed4::from_raw(-1);
-constexpr Fixed4 FALL_MAX_VEL = Fixed4::from_raw(-20);
+constexpr F4 JUMP_VEL = F4::from_raw(25);
+constexpr F4 JUMP_MIDAIR_ACCEL = F4::from_raw(1);
+constexpr F4 SPRING_VEL = F4::from_raw(38);
+constexpr F4 MOVE_ACCEL = F4::from_raw(3);
+constexpr F4 MOVE_ACCEL_WATER = F4::from_raw(2); // slower in water
+constexpr F4 MOVE_ACCEL_ICE = F4::from_raw(1);   // much slower on ice
+constexpr F4 MOVE_MAX_VEL = F4::from_raw(10);
+constexpr F4 GRAVITY = F4::from_raw(-2);
+constexpr F4 GRAVITY_WATER = F4::from_raw(-1);
+constexpr F4 FALL_MAX_VEL = F4::from_raw(-20);
 
 struct TilePos {
   uint8_t x;
@@ -124,12 +125,13 @@ constexpr int COIN_SPAWN_COUNT = count_coin_spawns();
 constexpr auto COIN_SPAWN_POSITIONS = get_coin_spawns<COIN_SPAWN_COUNT>();
 
 struct Player {
-  Fixed4 x = Fixed4::from_raw(0);
-  Fixed4 y = Fixed4::from_raw(0);
-  Fixed4 x_vel = Fixed4::from_raw(0);
-  Fixed4 y_vel = Fixed4::from_raw(0);
+  F4 x = F4::from_raw(0);
+  F4 y = F4::from_raw(0);
+  F4 x_vel = F4::from_raw(0);
+  F4 y_vel = F4::from_raw(0);
   int score{0};
   int dead_timeout{0};
+  bool queue_dead{false}; // this is NOT needed on FPGA, just a hack for CPU version
 };
 
 struct GameState {
@@ -148,7 +150,7 @@ struct PlayerInput {
 
 GameState init(uint64_t seed);
 void update(GameState &state, const PlayerInput &in1, const PlayerInput &in2);
-void observe_state_simple(const GameState &state, std::vector<Fixed4> &observation);
+void observe_state_simple(const GameState &state, std::vector<F4> &observation);
 void observe_state_screen(const GameState &state, std::vector<uint8_t> &observation);
 
 } // namespace jnb
